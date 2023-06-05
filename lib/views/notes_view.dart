926991efbca90cacd570ef77600aa7892035a3af
Omar Ashraf <omarashraffci@gmail.com>
2023-06-05 11:06:57 +0300

    Creating AppBar and the main body column

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/CustomAppBar.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
