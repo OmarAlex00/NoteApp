84a67c6d35804de6035e1c0d9bf34efbc1916b12
Omar Ashraf <omarashraffci@gmail.com>
2023-06-06 12:37:50 +0300

    adding modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/BottomSheet.dart';
 import 'package:note_app/views/widgets/noteViewBody.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,8 +8,15 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const AddNotesBottomSheet();
+                });
+          },
+          child: const Icon(Icons.add)),
       body: const NoteViewBody(),
     );
   }
