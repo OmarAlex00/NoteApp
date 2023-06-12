ba8dfc562423dc95047bde219ea22292ecf9c088
Omar Ashraf <omarashraffci@gmail.com>
2023-06-12 11:26:27 +0300

    Edit note screen on clicking on any note

--- lib/views/widgets/NoteCard.dart
+++ lib/views/widgets/NoteCard.dart
@@ -1,50 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/Screens/Edit_note_screen.dart';
 
 class NoteCard extends StatelessWidget {
   const NoteCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 20, left: 16, bottom: 22, right: 5),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCD7A),
-          borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 24),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EditNoteScreen();
+          }),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 20, left: 16, bottom: 22, right: 5),
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCD7A),
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  'Build your career with Omar Ashraf',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.5), fontSize: 18),
+                ),
+              ),
+              trailing: const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Icon(
+                  FontAwesomeIcons.trash,
+                  size: 20,
+                ),
+              ),
+              iconColor: Colors.black,
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
               child: Text(
-                'Build your career with Omar Ashraf',
+                'May 21, 2022',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(.5), fontSize: 18),
+                    color: Colors.black.withOpacity(.4), fontSize: 12),
               ),
-            ),
-            trailing: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Icon(
-                FontAwesomeIcons.trash,
-                size: 20,
-              ),
-            ),
-            iconColor: Colors.black,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Text(
-              'May 21, 2022',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 12),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
