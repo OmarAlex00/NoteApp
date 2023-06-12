a67dcf21d42f04fed9b5b2f15b8d37c5b39f5289
Omar Ashraf <omarashraffci@gmail.com>
2023-06-12 11:14:45 +0300

    bottom sheet is scrollable

--- lib/views/widgets/BottomSheet.dart
+++ lib/views/widgets/BottomSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/views/widgets/CustomTextField.dart';
 
 class AddNotesBottomSheet extends StatelessWidget {
@@ -6,20 +7,47 @@ class AddNotesBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 25),
-          CustomTextField(hint: 'Title'),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxLines: 5,
-          )
-        ],
+    return Padding(
+      padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+          bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: const SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 25),
+            CustomTextField(hint: 'Title'),
+            SizedBox(height: 20),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(height: 100),
+            CustomButton(),
+            SizedBox(height: 16)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
+        ),
       ),
     );
   }
