8c331f928ffff26692be5875d19f29194bfa284c
Omar Ashraf <omarashraffci@gmail.com>
2023-06-14 14:34:11 +0300

    add bloc observer

--- lib/views/widgets/bottom_sheet.dart
+++ lib/views/widgets/bottom_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note/add_note_cubit.dart';
 import 'package:note_app/views/widgets/add_note_form.dart';
 
 class AddNotesBottomSheet extends StatelessWidget {
@@ -11,8 +14,21 @@ class AddNotesBottomSheet extends StatelessWidget {
           left: 16,
           right: 16,
           bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: const SingleChildScrollView(
-        child: NoteForm(),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print('Something went wrong ${state.errorMessage}');
+            } else if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return const ModalProgressHUD(
+                inAsyncCall: State is AddNoteLoading ? true : false,
+                child: NoteForm());
+          },
+        ),
       ),
     );
   }
