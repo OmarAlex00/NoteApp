13797957c11f107fc302583cc31a9db7c05d5401
Omar Ashraf <omarashraffci@gmail.com>
2023-06-18 16:29:26 +0300

    solving warrnings

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/Helper/test_bloc_observer.dart';
 import 'package:note_app/Models/note_model.dart';
 import 'package:note_app/constants.dart';
-import 'package:note_app/cubits/add_note/add_note_cubit.dart';
 import 'package:note_app/views/Screens/notes_view.dart';
 
 void main() async {
